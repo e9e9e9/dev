33155844b7d190d679ced7ec8e9987592f867d32
e9e9e9 <78357762+e9e9e9@users.noreply.github.com>
2022-05-29 04:53:53 +0900

    Add Snippet

---
+++ Snippet/simple_image_download.py
@@ -0,0 +1,11 @@
+from simple_image_download import simple_image_download as sid
+
+response = sid.simple_image_download
+
+
+# response().download('세탁기', 100)
+# response().download('냉장고', 100)
+# response().download('에어컨', 100)
+# response().download('청소기', 100)
+# response().download('공기청정기', 100)
+response().download('로봇청소기', 100)
