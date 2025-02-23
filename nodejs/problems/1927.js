03e9dc473cbc43d23c181b9b7a83a643755ba856
e9e9e9 <78357762+e9e9e9@users.noreply.github.com>
2025-02-23 12:19:08 +0900

    solve problems

---
+++ nodejs/problems/1927.js
@@ -0,0 +1,81 @@
+class Heap {
+  constructor() {
+    this.arr = [];
+  }
+
+  push(num) {
+    this.arr.push(num);
+    let idx = this.arr.length - 1;
+    let parentIdx = Math.floor((idx - 1) / 2);
+    while (idx > 0 && this.arr[parentIdx] > this.arr[idx]) {
+      [this.arr[idx], this.arr[parentIdx]] = [
+        this.arr[parentIdx],
+        this.arr[idx],
+      ];
+      idx = parentIdx;
+      parentIdx = Math.floor((idx - 1) / 2);
+    }
+    // console.log('push', this.arr);
+  }
+
+  pop() {
+    if (this.arr.length <= 0) {
+      return 0;
+    }
+    const result = this.arr[0];
+
+    if (this.arr.length === 1) {
+      this.arr.pop();
+      return result;
+    }
+
+    this.arr[0] = this.arr.pop();
+    let idx = 0;
+
+    while (true) {
+      const left = 2 * idx + 1;
+      const right = 2 * idx + 2;
+      let smallestIdx = idx;
+
+      if (left < this.arr.length && this.arr[left] < this.arr[smallestIdx]) {
+        smallestIdx = left;
+      }
+      if (right < this.arr.length && this.arr[right] < this.arr[smallestIdx]) {
+        smallestIdx = right;
+      }
+      if (smallestIdx !== idx) {
+        [this.arr[idx], this.arr[smallestIdx]] = [
+          this.arr[smallestIdx],
+          this.arr[idx],
+        ];
+        idx = smallestIdx;
+      } else {
+        // console.log('pop', this.arr);
+        return result;
+      }
+    }
+  }
+}
+
+const input = require('fs')
+  .readFileSync(
+    process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt'
+  )
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number);
+
+const heap = new Heap();
+const nums = input.slice(1);
+const results = [];
+
+for (const num of nums) {
+  if (num === 0) {
+    results.push(heap.pop());
+  } else {
+    heap.push(num);
+  }
+}
+
+console.log(results.join('\n'));
